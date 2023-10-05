e7ee68063fa5de039d0c26b4aa699b811cdbcae4
Jéssica Rocha Ferreira Rodrigues <jeskrfr@gmail.com>
2023-10-05 12:50:48 -0300

    teste

---
+++ App.js
@@ -0,0 +1,39 @@
+//import { StyleSheet, View} from 'react-native';
+//import HelloRN from './components/01HelloRNV1';
+//import HelloRNV2 from './components/02HelloRNV2';
+//import MyCat from './components/03MyCat';
+//import MyCat from './components/04MyCat';
+//import Parent from './components/05Nested/05Parent';
+//import State from './components/06State';
+//import estilos from './css/MyCSS';
+//import MyTextInput from './components/07TextInput';
+//import JuntarFrases from './components/08JuntarFrases';
+//import Calculadora from './components/09Calculadora/09Caculadora';
+//import MyScrollView from './components/10MyScrollView';
+//import MyFlatList from './components/11MyFlatList';
+//import MySectionList from './components/12SectionList';
+//import MyFlexBox from './components/13Flexbox/13Flexbox__';
+//import MyFlexBox from './components/13Flexbox/13FlexboxA';
+//import MyFlexBox from './components/13Flexbox/13FlexboxB';
+//import MyNetworking from './components/14Networking/14MyNetworking';
+//import MainNavApp from './components/15Navigation/01NavStack/MainNavApp';
+//import MainScreen from './components/15NavigationAula/15NavStack/MainScreen';
+//import PokemonApp from './components_prof/15Navigation/05Pokemon/PokemonApp';
+import PokemonApp from "./components/16Pokemon/PokemonApp";
+//import MyIconButton from "./components/17IconButton/MyIconButton";
+
+export default function App() {
+  return (
+    <PokemonApp/>
+  );
+}
+
+/*
+
+                   ---> ChildA
+App ---> Parent---> 
+                   ---> ChildB
+
+
+*/
+
